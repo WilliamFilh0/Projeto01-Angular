5720d07d2041bd3d30bfada07450b5427ac8a6a6
WilliamFilh0 <williamfilho8908@gmail.com>
2025-09-26 23:10:34 -0300

    Criando um novo componente

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NewComponent } from './components/new-component/new-component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  template: `<h1>Curso de Angular</h1> `,
+  imports: [RouterOutlet, CommonModule, NewComponent],
+  template: `<h1>Curso de Angular</h1>
+    <app-new-component /> `,
 })
 export class App {
   protected readonly title = signal('meu-primeiro-projeto-latest');
