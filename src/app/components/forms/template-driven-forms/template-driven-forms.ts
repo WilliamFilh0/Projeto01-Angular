c26a3dbd4051d7c0af35b487b75d4b75cf0b831c
William <william.araujo@zcr.com.br>
2025-10-31 14:11:56 -0300

    Form input - Teplate driven-forms

--- src/app/components/forms/template-driven-forms/template-driven-forms.ts
+++ src/app/components/forms/template-driven-forms/template-driven-forms.ts
@@ -1,11 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-template-driven-forms',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './template-driven-forms.html',
-  styleUrl: './template-driven-forms.scss'
+  styleUrl: './template-driven-forms.scss',
 })
-export class TemplateDrivenForms {
-
-}
+export class TemplateDrivenForms {}
