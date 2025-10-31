c3011772346f8eb15b296095e879d6a4a4c1e73a
William <william.araujo@zcr.com.br>
2025-10-31 15:39:21 -0300

    Select

--- src/app/components/forms/template-driven-forms/template-driven-forms.ts
+++ src/app/components/forms/template-driven-forms/template-driven-forms.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
@@ -8,4 +8,19 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './template-driven-forms.html',
   styleUrl: './template-driven-forms.scss',
 })
-export class TemplateDrivenForms {}
+export class TemplateDrivenForms {
+  public listaComidas = signal<Array<{ comida: string; preco: string }>>([
+    {
+      comida: 'Xburguer',
+      preco: 'R$ 10',
+    },
+    {
+      comida: 'XSalada',
+      preco: 'R$ 11',
+    },
+    {
+      comida: 'Coxinha',
+      preco: 'R$ 6',
+    },
+  ]);
+}
